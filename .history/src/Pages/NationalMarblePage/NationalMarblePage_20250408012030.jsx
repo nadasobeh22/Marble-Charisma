883228918422210cce23daaa9be0ee1098e3062e
nadasobeh22 <sobehnada2@gmail.com>
2025-04-08 01:22:26 +0300

    init

--- .history/src/Pages/NationalMarblePage/NationalMarblePage_20250408012030.jsx
+++ .history/src/Pages/NationalMarblePage/NationalMarblePage_20250408012030.jsx
@@ -11,7 +11,7 @@ const NationalMarblePage = () => {
     <section className="container mx-auto px-4 sm:px-6 pt-4 sm:pt-6 lg:pt-8 pb-4 sm:pb-6 lg:pb-8 bg-offwhite dark:bg-darkslate transition-colors duration-300">
       <div className="text-center mb-2 sm:mb-3 lg:mb-4">
         <h1 className="text-2xl sm:text-3xl lg:text-4xl font-extrabold text-center font-poppins drop-shadow-lg text-teal dark:text-teal animate-pulse">
-        الرخام الوطني السوري  [ بكافة السماكات والمقاييس والأشكال ]
+           [ ]الرخام الوطني السوري
         </h1>
         
       </div>
